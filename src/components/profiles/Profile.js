f97d8b1989c81ec7594d58b54f745e90bb27b6d6
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-21 14:17:15 +0430

    Add missions in profile part

--- src/components/profiles/Profile.js
+++ src/components/profiles/Profile.js
@@ -1,5 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Profile = () => <div>Profile</div>;
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.joined);
+
+  return (
+    <>
+      <section className="profile_container">
+
+        <section className="myMissions">
+          <h2 className="sectionTitle">My Missions</h2>
+          {joinedMissions.length > 0 ? (
+            <ul className="myMissionsList exist">
+              {joinedMissions.map((mission) => {
+                if (mission.joined) {
+                  return (
+                    <li className="item" key={mission.id}>
+                      {mission.name}
+                    </li>
+                  );
+                }
+                return null;
+              })}
+            </ul>
+          ) : (
+            <div className="myMissionsList none">
+              <p>No missions Joined</p>
+            </div>
+          )}
+        </section>
+      </section>
+    </>
+  );
+};
 
 export default Profile;
